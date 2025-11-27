e65f846eeb215f889a2901980a1c170231aa0f50
npjohnson <johnsonnolen@gmail.com>
2025-11-27 12:06:55 -0500

    Galaxian: Uprev keymint dependency from V3-ndk to V4-ndk
    * QPR2 doesn't let us link V3-ndk anymore.
    
    Change-Id: Id174fd55caec2466c7812d8cb77bdf5f0cb36531

--- extract-files.py
+++ extract-files.py
@@ -16,6 +16,20 @@ namespace_imports = [
     'hardware/mediatek',
 ]
 
+
+def lib_fixup_vendor_suffix(lib: str, partition: str, *args, **kwargs):
+    return f'{lib}_{partition}' if partition == 'vendor' else None
+
+
+lib_fixups: lib_fixups_user_type = {
+    **lib_fixups,
+}
+
+blob_fixups: blob_fixups_user_type = {
+'vendor/bin/hw/android.hardware.security.keymint@3.0-service.trustonic': blob_fixup()
+        .replace_needed('android.hardware.security.keymint-V3-ndk.so', 'android.hardware.security.keymint-V4-ndk.so'),
+}  # fmt: skip
+
 module = ExtractUtilsModule(
     'galaxian',
     'nothing',
