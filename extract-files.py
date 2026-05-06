7f3f6c40a927fd3e9d3ac508d92bafe5f6044acf
Andreyka445 <seleznevandrey186@outlook.com>
2026-05-06 20:04:07 +0700

    Galaxian: CleanUP!
    Signed-off-by: Andreyka445 <seleznevandrey186@outlook.com>

--- extract-files.py
+++ extract-files.py
@@ -4,6 +4,14 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+from extract_utils.fixups_blob import (
+    blob_fixup,
+    blob_fixups_user_type,
+)
+from extract_utils.fixups_lib import (
+    lib_fixups,
+    lib_fixups_user_type,
+)
 from extract_utils.main import (
     ExtractUtils,
     ExtractUtilsModule,
